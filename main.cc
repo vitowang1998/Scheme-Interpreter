c2a55670177d2f5901ec958e4fd540146ef47d6b
Haoran Wang <vitowang1998@gmail.com>
2018-09-24 00:32:09 -0400

    Adds the main file to the reposiotry.

---
+++ main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << "---------------------------------------------------------------" << endl;
+    cout << "Welcome to Scheme." << endl;
+    cout << "Hit Control+C to exit." << endl;
+
+    return 0;
+}
